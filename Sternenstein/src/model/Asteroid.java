d9fc1c4cffcf396bb9a00501bfd5f82e3cae17db
lauren35 <147293452+lauren35@users.noreply.github.com>
2023-10-07 23:10:44 -0500

    Add files via upload

---
+++ Sternenstein/src/model/Asteroid.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ * Represents the Asteroid on game play screen
+ *
+ *return a drawing of the asteroid
+ */
+public class Asteroid {
+    
+  boolean collided;
+  boolean destroyed;
+    
+    
+  /**
+   * Checks to see if asteroid has collided with anything
+   * 
+   * return 
+   */
+    
+}
---
+++ Sternenstein/src/model/Sore.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ * Calculates and holds the player's score 
+ * 
+ * returns an integer score value
+ */
+public class Sore {
+    
+    int score;
+    
+    /**
+     * Updates score if player shoots an asteroid
+     * 
+     * @return an integer value 
+     */
+     int addScore(){
+        //TODo implement addScore method
+        //TODO remve dummy return
+        return score;
+    }
+    
+}
