11f86549908d6cbc18470655a312e9756d01f03b
laure <laure@10.0.0.47>
2023-10-10 20:36:03 -0500

    update life class

--- src/model/Life.java
+++ src/model/Life.java
@@ -5,9 +5,32 @@
 package model;
 
 /**
- *
- * @author laure
+ *Represents the player's life count.
+ * 
+ * return an integer value between 0 and 3
  */
 public class Life {
     
+    int lifeCount;
+    
+    /**
+     * Decreases the player's life count when they collide with an asteroid
+     * 
+     * return integer value that is one less than the previous life value
+     */
+    int decrease(int lifeCount){
+        //TODO implement decrease method
+        return lifeCount; 
+    }
+    
+    /**
+     * Will trigger the game over sequence when all lives are lost
+     * 
+     * return void
+     */
+    
+    void onGameOver(){
+        //TODO implement onGameOver
+        
+    }
 }
