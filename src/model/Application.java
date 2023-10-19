f196cc7ee3bb681f15d982f4c74d4c9516d62704
laure <laure@10.0.0.47>
2023-10-18 22:01:23 -0500

    final changes before code drop 10/18

--- src/model/Application.java
+++ src/model/Application.java
@@ -5,11 +5,16 @@
 package model;
 
 /**
+ * Connor had trouble pushing to the GitHub repo. He has started work on the
+ * player class, and we will work on fixing the push issue with the new repo. We
+ * are having to change to Maven to use JavaFX
+ */
+/**
  * runs the main application window
- * 
+ *
  */
 public class Application {
-    
+
     boolean isRunning;
-    
+
 }
